3c275c0824e4822ff0c88e426fed45a10736b85d
Fahad-sec <fahadakhtarabbasi@gmail.com>
2026-02-26 17:28:14 +0500

    fix: v0.4 Improve Obstacle spawns

--- Assets/PipeSpawner.cs
+++ Assets/PipeSpawner.cs
@@ -6,9 +6,14 @@ public class PipeSpawner : MonoBehaviour
 {
    public GameObject pipePrefab;
     public float spawnRate = 2f;
-    public float heightOffset = 1f;
+    public float heightOffset = 10;
     private float timer = 0;
     public LogicManager logic;
+
+    void Start()
+    {
+        SpawnPipe();
+    }
 
     void Update()
     {
@@ -18,10 +23,17 @@ public class PipeSpawner : MonoBehaviour
 
         }else
         {
-            Instantiate(pipePrefab, transform.position, transform.rotation);
+            SpawnPipe();
             timer = 0;
 
         }
     }
 
+    void SpawnPipe()
+    {
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+        Vector2 RandomPos = new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0);
+        Instantiate(pipePrefab, RandomPos, transform.rotation);
+    }
 }
